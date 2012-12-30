17917ddf5abc6eeff506e1b9c16ea0f6969a8ddc
Ludo Stellingwerff <ludo@almende.org>
2012-12-30 17:24:46 +0100

    Various fixes to make site better visible on search engines, updated Eve to try to reduce logging somewhat.

--- src/org/stwerff/mslagents/SpiceAgent.java
+++ src/org/stwerff/mslagents/SpiceAgent.java
@@ -1,8 +1,11 @@
 package org.stwerff.mslagents;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
 import org.stwerff.mslagents.data.Image;
 
 import com.almende.eve.agent.Agent;
@@ -19,10 +22,26 @@ public class SpiceAgent extends Agent {
 			for (int i=0; i<list.size(); i++){
 				Image image = om.treeToValue(list.get(i),Image.class);
 				if (image.getLmst() == null){
-					Process proc = Runtime.getRuntime().exec("pwd", null,null);
-					System.err.println(new BufferedReader(new InputStreamReader(proc.getInputStream())).readLine());
+					String path = getSession().getRequest().getServletContext().getRealPath("/");
+					path+="/c_bin/";
+					File pathFile = new File(path);
+					File test = new File(path+"msl_lmst");
+					if (!test.canExecute()){
+						Runtime.getRuntime().exec("chmod +x msl_lmst",null,pathFile);
+					}
+					DateTime time = new DateTime(image.getUnixTimeStamp());
+					Process proc = Runtime.getRuntime().exec("./msl_lmst "+time.toString(ISODateTimeFormat.dateHourMinuteSecond()), null,pathFile);
+					String lmst = new BufferedReader(new InputStreamReader(proc.getInputStream())).readLine();
 					proc.destroy();
-					//result.add(om.valueToTree(image));
+					String[] fields = lmst.split(":");
+					String solString = String.format("%05d", image.getSol());
+					if (("1/"+solString).equals(fields[0])){
+						image.setLmst(fields[1]+":"+fields[2]+":"+fields[3]);	
+					} else {
+						System.err.println("Failed to convert to correct LMST, wrong sol?");
+					}
+					
+					result.add(om.valueToTree(image));
 				}
 			}
 		} catch (Exception e) {
